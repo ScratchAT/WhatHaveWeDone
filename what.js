21549eaeb33f78800311a754166d7ee843198a79
DatOneLefty <k9dogs101@gmail.com>
2017-08-12 15:01:06 -0400

    what.js

--- what.js
+++ what.js
@@ -22,17 +22,19 @@ app.get('/', function(req, res){
 
 
 
-var onevent = socket.onevent;
-socket.onevent = function (packet) {
-    var args = packet.data || [];
-    onevent.call (this, packet);    // original call
-    packet.data = ["*"].concat(args);
-    onevent.call(this, packet);      // additional call to catch-all
-};
+
 
 io.on('connection', function(socket){
+  var onevent = socket.onevent;
+  socket.onevent = function (packet) {
+      var args = packet.data || [];
+      onevent.call (this, packet);    // original call
+      packet.data = ["*"].concat(args);
+      onevent.call(this, packet);      // additional call to catch-all
+  };
   socket.on("*",function(event,data) {
       io.emit(event, data);
+      console.log(event + ":" + data);
   });
 });
 
